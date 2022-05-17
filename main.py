708a97f13bd070d26687f2332193eb69c1943ffc
Adam <adam.cwikla99@gmail.com>
2022-05-18 00:38:52 +0200

    xor testing example and character recognition initial code - there is a need for loading and saving already trained network

--- main.py
+++ main.py
@@ -5,6 +5,49 @@ from LabelFile import LabelFile
 from neuralnetwork import NeuralNetwork
 import numpy as np
 
+from sklearn.metrics import classification_report
+
+def runProgram():
+    print("loading training data ")
+
+    # na czas testowania dałem dane do walidacje jako dane na podstawi których sieć się uczy
+    trainLabel = LabelFile('./data/train-labels-idx1-ubyte') 
+    trainImages = ImageFile('./data/train-images-idx3-ubyte')
+    #trainLabel = LabelFile('./data/t10k-labels-idx1-ubyte') 
+    #trainImages = ImageFile('./data/t10k-images-idx3-ubyte')
+
+    print("loading end ")
+    print("loaded training images {}".format(trainLabel.label_count))
+
+    print("loading testing data ")
+    testLabel = LabelFile('./data/t10k-labels-idx1-ubyte') 
+    testImages = ImageFile('./data/t10k-images-idx3-ubyte')
+    print("loading end ")
+    print("loaded testing images {}".format(testLabel.label_count))
+
+
+
+    print("array convertion")
+    xdI = np.array(trainImages.imagesOneDim)
+    xdITestowe = np.array(testImages.imagesOneDim)
+
+    xdL = np.array(trainLabel.testV2)
+    xdLTestowe = np.array(testLabel.testV2)
+    print("array ended")
+
+    print("creating network ")
+#    imageNetwork = NeuralNetwork([784, 392, 196, 48, 24, 10], alpha=0.2)
+#    imageNetwork = NeuralNetwork([xdI.shape[1], 392, 196, 10], alpha=0.2)
+    imageNetwork = NeuralNetwork([xdI.shape[1], 196, 10], alpha=0.2)
+    print("[INFO] {}".format(imageNetwork))
+
+    print("network training started")
+    imageNetwork.fit(xdI, xdL, epochs=100)
+    print("network training ended")
+
+    predictions = imageNetwork.predict(xdITestowe)
+    predictions = predictions.argmax(axis=1)
+    print(classification_report(xdLTestowe.argmax(axis=1), predictions))
 
 
 def test():
@@ -14,6 +57,7 @@ def main():
     1
     print("test")
     test()
+    runProgram()
 
  
     #region xor network usage example
